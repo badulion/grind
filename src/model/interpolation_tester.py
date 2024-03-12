1a11674ac79c8d0fb574e13820bb0c0a34ab42e2
Andrzej Dulny <andzej.dulny@gmail.com>
2024-03-12 12:12:07 +0100

    Interpolation experiment

--- src/model/interpolation_tester.py
+++ src/model/interpolation_tester.py
@@ -19,52 +19,49 @@ class InterpolationTester(pl.LightningModule):
                  *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.save_hyperparameters()
+        self.min_ks = 3
+        self.max_ks = 25
         
-        self.fourier_interpolator = FourierInterpolator(num_ks=num_ks, spatial_dim=spatial_dim)
-        
-    def forward(self, x_cloud, p_cloud, p_grid):
+    def forward(self, x_cloud, p_cloud, p_grid, ks: int = 10):
         # check devices
         interpolation_points = p_grid.view(p_grid.shape[0], -1, 2)
+
+        # interpolation layer 
+        interpolation_layer = FourierInterpolator(num_ks=ks, spatial_dim=2)
             
         # interpolate on a grid
-        x_grid = self.fourier_interpolator(p_cloud, x_cloud, interpolation_points)
+        x_grid = interpolation_layer(p_cloud, x_cloud, interpolation_points)
         x_grid = x_grid.view(x_cloud.shape[0], p_grid.shape[1], p_grid.shape[2], x_cloud.shape[-1])
         x_grid = x_grid.permute(0, 3, 1, 2)
         return x_grid
         
-        # resnet smoother
-        if hasattr(self, "smoother"):
-            x_grid = x_grid + self.smoother(x_grid)
-        
-        # run solver
-        x_pred = self.solver_net(x_grid, t_eval=t_eval)
-        
-        # interpolate back to the original points
-        x_pred = x_pred.permute(1, 0, 3, 4, 2)
-        x_pred = x_pred.reshape(x.shape[0], len(t_eval[1:]), self.grid_resolution**2, x.shape[-1])
-        x_pred = self.fourier_interpolator(self.interpolation_points.view(1,1,*self.interpolation_points.shape), x_pred, p.unsqueeze(1))
-        return x_pred
-        
     def step(self, batch, batch_idx, stage):
         (x_cloud, p_cloud, x_grid, p_grid) = batch
-        x_interpolated = self(x_cloud, p_cloud, p_grid)
-        loss = F.mse_loss(x_interpolated, x_grid)
-        return loss
+        losses = {}
+        for ks in range(self.min_ks, self.max_ks+1):
+            x_interpolated = self(x_cloud, p_cloud, p_grid, ks=ks)
+            loss = F.mse_loss(x_interpolated, x_grid)
+            losses[f"ks_{ks}"] = loss
+        return losses
     
     def training_step(self, batch, batch_idx):
-        loss = self.step(batch, batch_idx, "train")
-        self.log("train_loss", loss, prog_bar=True)
-        return loss
+        losses = self.step(batch, batch_idx, "train")
+
+        for key, value in losses.items():
+            self.log(f"train_{key}", value)
+        return losses["ks_10"]
     
     def validation_step(self, batch, batch_idx):
-        loss = self.step(batch, batch_idx, "val")
-        self.log("val_loss", loss)
-        return loss
+        losses = self.step(batch, batch_idx, "val")
+        for key, value in losses.items():
+            self.log(f"val_{key}", value)
+        return losses["ks_10"]
     
     def test_step(self, batch, batch_idx):
-        loss = self.step(batch, batch_idx, "test")
-        self.log("test_loss", loss)
-        return loss
+        losses = self.step(batch, batch_idx, "test")
+        for key, value in losses.items():
+            self.log(f"test_{key}", value)
+        return losses["ks_10"]
         
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters())
